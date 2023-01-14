73668fbe7b7acec8f85b2ea6c7748f3ecd89ecf8
super1207 <1875159423@qq.com>
2023-01-14 16:43:26 +0800

    暂时停用【网页截图】命令，因为在某些网络环境下会编译失败

--- src/redlang/exfun.rs
+++ src/redlang/exfun.rs
@@ -2,7 +2,6 @@ use std::{path::Path, io::Read, time::{SystemTime, Duration}, collections::BTree
 
 use chrono::TimeZone;
 use encoding::Encoding;
-use headless_chrome::Browser;
 use md5::{Md5, Digest};
 use urlencoding::encode;
 use base64;
@@ -734,40 +733,6 @@ pub fn init_ex_fun_map() {
         return Ok(Some("".to_string()));
     });
     add_fun(vec!["网页截图"],|self_t,params|{
-        fn access(self_t:&mut RedLang,params: &[String]) -> Result<Option<String>, Box<dyn std::error::Error>> {
-            let path = self_t.get_param(params, 0)?;
-            let sec = self_t.get_param(params, 1)?;
-            let mut arg_vec:Vec<&std::ffi::OsStr> = vec![];
-            let proxy_str = self_t.get_coremap("代理")?;
-            let proxy:std::ffi::OsString;
-            if proxy_str != "" {
-                proxy = std::ffi::OsString::from("--proxy-server=".to_owned() + proxy_str);
-                arg_vec.push(&proxy);
-            }
-            let options = headless_chrome::LaunchOptions::default_builder()
-                .window_size(Some((1920, 1080)))
-                .args(arg_vec)
-                .build()?;
-                let browser = Browser::new(options)?;
-                let tab = browser.wait_for_initial_tab()?;
-                tab.navigate_to(&path)?.wait_until_navigated()?;
-            let el_html= tab.wait_for_element("html")?;
-            let body_height = el_html.get_box_model()?.height;
-            let body_width = el_html.get_box_model()?.width;
-            tab.set_bounds(headless_chrome::types::Bounds::Normal { left: Some(0), top: Some(0), width:Some(body_width), height: Some(body_height) })?;
-            let mut el = el_html;
-            if sec != ""{
-                el = tab.wait_for_element(&sec)?;
-            }
-            let png_data = tab.capture_screenshot(headless_chrome::protocol::cdp::Page::CaptureScreenshotFormatOption::Png,
-                None,
-                Some(el.get_box_model()?.content_viewport()),
-                true)?;
-            return Ok(Some(self_t.build_bin(png_data)));
-        }
-        if let Ok(ret) = access(self_t,params){
-            return Ok(ret);
-        }
         return Ok(Some(self_t.build_bin(vec![])));
     });
     add_fun(vec!["命令行"],|self_t,params|{
