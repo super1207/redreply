11bc42cc1d5c0db77b3a41c487bec60c2526a1bb
super1207 <1875159423@qq.com>
2023-05-25 17:15:29 +0800

    优化【上传文件】

--- src/redlang/exfun.rs
+++ src/redlang/exfun.rs
@@ -2118,11 +2118,8 @@ pub fn init_ex_fun_map() {
             let mut data:Vec<u8> = vec![];
             data.append(&mut "--".as_bytes().to_owned());
             data.append(&mut bound.as_bytes().to_owned());
-            data.append(&mut "\r\nContent-Disposition: form-data; name=\"reqtype\"\r\n\r\nfileupload\r\n".as_bytes().to_owned());
-            data.append(&mut "--".as_bytes().to_owned());
-            data.append(&mut bound.as_bytes().to_owned());
             let fname = urlencoding::encode(filename).to_string();
-            data.append(&mut format!("\r\nContent-Disposition: form-data; name=\"fileToUpload\";filename=\"{fname}\"\r\n\r\n").as_bytes().to_owned());
+            data.append(&mut format!("\r\nContent-Disposition: form-data; name=\"files[]\";filename=\"{fname}\"\r\n\r\n").as_bytes().to_owned());
             data.append(file_data);
             data.append(&mut "\r\n--".as_bytes().to_owned());
             data.append(&mut bound.as_bytes().to_owned());
@@ -2146,12 +2143,15 @@ pub fn init_ex_fun_map() {
                 };
                 return ret;
             });
-            Ok(String::from_utf8(content)?)
+            let err = "json解析失败";
+            let js:serde_json::Value = serde_json::from_str(&String::from_utf8(content)?)?;
+            let url = js.get("files").ok_or(err)?.get(0).ok_or(err)?.get("url").ok_or(err)?.as_str().ok_or(err)?;
+            Ok(url.to_owned())
         }
         let bin_text = self_t.get_param(params, 0)?;
         let filename = self_t.get_param(params, 1)?;
         let mut bin = RedLang::parse_bin(&bin_text)?;
-        let url = "https://catbox.moe/user/api.php";
+        let url = "https://up1.fileditch.com/upload.php";
         match access(self_t,&filename,&url,&mut bin) {
             Ok(ret) => Ok(Some(ret)),
             Err(err) => {
