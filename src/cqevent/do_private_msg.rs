3def5d65e9fc8b5893366184767ccd55daaa652f
super1207 <pengyao1210@gmail.com>
2025-09-15 18:49:22 +0800

    私聊事件并发处理

--- src/cqevent/do_private_msg.rs
+++ src/cqevent/do_private_msg.rs
@@ -1,6 +1,6 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, rc::Rc, cell::RefCell};
 
-use crate::{cqapi::*, mytool::{json_to_cq_str, read_json_str}, read_code_cache, redlang::RedLang, status::add_recv_private_msg, G_INPUTSTREAM_VEC};
+use crate::{cqapi::*, mytool::{json_to_cq_str, read_json_str}, read_code_cache, redlang::RedLang, status::add_recv_private_msg, G_INPUTSTREAM_VEC, RT_PTR};
 
 use super::{is_key_match, get_script_info, set_normal_message_info};
 
@@ -38,14 +38,22 @@ fn do_redlang(root: &serde_json::Value,ban_pkgs:&HashSet<String>) -> Result<(), 
         }
         let mut rl = RedLang::new();
         if cffs == "私聊触发" || cffs == "群、私聊触发"{
-            rl.set_exmap("当前消息",&msg)?;
             set_normal_message_info(&mut rl, root)?;
+            rl.set_exmap("当前消息",&msg)?;
             if is_key_match(&mut rl,&ppfs,keyword,&msg)? {
-                rl.script_name = name.to_owned();
-                rl.pkg_name = pkg_name.to_owned();
-                if let Err(e) = super::do_script(&mut rl,code,"normal",false) {
-                    cq_add_log_w(format!("err in do_private_msg:do_redlang:{}", e.to_string()).as_str()).unwrap();
-                }
+                let exmap = (*rl.exmap).borrow().clone();
+                let code_t = code.to_owned();
+                let pkg_name_t = pkg_name.to_owned();
+                let script_name_t = name.to_owned();
+                RT_PTR.spawn_blocking(move ||{
+                    let mut rl = RedLang::new();
+                    rl.exmap = Rc::new(RefCell::new(exmap));
+                    rl.pkg_name = pkg_name_t.to_owned();
+                    rl.script_name = script_name_t.to_owned();
+                    if let Err(e) = super::do_script(&mut rl,&code_t,"normal",false) {
+                        cq_add_log_w(format!("err in do_private_msg:do_redlang:{}", e.to_string()).as_str()).unwrap();
+                    }
+                });
             }
         }
     }
